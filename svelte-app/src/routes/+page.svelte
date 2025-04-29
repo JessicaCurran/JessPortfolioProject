92c9d27d7e4addcdb39a6bc735ec8ce6b028b57d
unknown <c00300855@setu.ie>
2025-04-29 12:21:15 +0100

    home page design done

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,2 +1,188 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<style>
+    h1 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 0%;
+        left: 38%;
+        text-align: center;
+        font-weight: bold;
+        font-size: 70px;
+        color: rgb(131, 38, 139);
+    }
+
+    h2 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 15%;
+        left: 10%;
+        text-align: left;
+        font-weight: bold;
+        font-size: 50px;
+        color: rgb(131, 38, 139);
+ 
+    }
+
+    h3 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 14%;
+        left: 40%;
+        text-align: center;
+        font-weight: bold;
+        font-size: 50px;
+        color: rgb(131, 38, 139);
+    }
+
+   h4 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 12%;
+        right: 10%;
+        text-align: right;
+        font-weight: bold;
+        font-size: 50px;
+        color: rgb(131, 38, 139);
+    }
+
+    h5 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 32%;
+        left: 26%;
+        font-weight: bold;
+        font-size: 40px;
+        color: rgb(255, 255, 255);
+    }
+
+    h6 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 40%;
+        left: 26%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+    }
+
+    h7 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 70%;
+        left: 26%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+    }
+
+    h8 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 100%;
+        left: 3%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+    }
+
+    .background {
+         background-image: url('cloud.jpg');
+         background-size: cover;
+         background-position: center;
+         background-repeat: no-repeat;
+         height: 150vh;
+        
+    }
+
+    hr {
+        border: 3px solid black;
+        width: 98.8%;
+        position: absolute;
+        top: 160px;
+    }
+    .second-line {
+        top: 258px;
+    }
+
+    .vertical-line {
+        width: 5px;
+        height: 100px;
+        background-color: black;
+        position: absolute;
+        top: 18%;
+        left: 30%;
+    }
+
+     .vertical-line2 {
+        width: 5px;
+        height: 100px;
+        background-color: black;
+        position: absolute;
+        top: 18%;
+        left: 67%;
+    }
+
+    
+    .drawing1 {
+         position: absolute;
+         top: 40%;
+         left: 5%;
+         width: 300px;
+         z-index: 1;
+        
+    }
+
+    .home {
+         position: absolute;
+         top: 1%;
+         left: 2%;
+         width: 150px;
+         z-index: 1;
+    }
+
+    .drawing2 {
+         position: absolute;
+         top: 90%;
+         right: 10%;
+         width: 400px;
+         z-index: 1;
+        
+    }
+ 
+
+  
+
+
+</style>
+
+<hr/> 
+
+
+<h1>Jess' Site</h1>
+<h2>My CV</h2>
+<h3>About me</h3>
+<h4>My work</h4>
+<h5>Hello!</h5>
+<h6>My name is Jess and this is my website. I am 21 and am currently studying digital art and design. I have 
+    always had a big passion for art and all things creative. For most of my life i have done traditional drawings with pen and paper but since starting this course, i've started exploring more digital art which i would like to learn more and get better at.</h6>
+<h7>Im not sure exactly what i would like to do in the future but
+    i would love to be involved with something creative and 
+    possibly art related.</h7>
+    <h8> This is my website where i will showcase some of the work i have
+         done <br> over the years including drawings, games and digital artwork. My most <br> prefered art style has always been on paper using lots of colour. 
+        Since <br> starting this course, i have discovered and learned all about digital art <br> and have really enjoyed learning and experimenting with all of the <br>
+    different ways to create digital art.</h8>
+<div class= "background">
+</div>
+<img src= "tokyo ghoul complete.png" alt= "drawing1" class= "drawing1"/>
+<img src= "home.png" alt= "homeButton" class= "home"/>
+<img src= "drawing2.jpg" alt= "drawing2" class= "drawing2"/>
+<hr class= "second-line" />
+<div class= "vertical-line">
+</div>
+<div class= "vertical-line2">
+</div>
