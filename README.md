549cda47d5b21e29e8e86d3680c8de0ab0e9c3d7
unknown <c00300855@setu.ie>
2025-05-02 20:25:37 +0100

    college work pages complete

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JessPortfolioProject 
 # C00300855 - Student ID
-# Submission date - 1/05/25
+# Submission date - 4/05/25
 
 # Help I got with code
 Previous group project creating our site
---
+++ svelte-app/src/routes/Games/+page.svelte
@@ -0,0 +1,137 @@
+<style>
+
+.background {
+    background-image: url('cloud.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 370vh;
+   
+}
+
+h1 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 4%;
+        left: 42%;
+        text-align: center;
+        font-weight: bold;
+        font-size: 70px;
+        color: rgb(131, 38, 139);
+    }
+
+    .line1 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 45%;
+        left: 15%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+}
+
+    .video1 {
+        width: 1000px;
+        position:absolute;
+        top: 75%;
+        left: 25%;
+    }
+    
+    .line2 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 145%;
+        left: 22%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+}
+
+    .video2 {
+        width: 1000px;
+        position:absolute;
+        top: 175%;
+        left: 25%;
+    }
+
+    .line3 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 245%;
+        left: 19%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+}
+
+.video3 {
+        width: 1000px;
+        position:absolute;
+        top: 275%;
+        left: 25%;
+    }
+
+    .line4 {
+        font-family: "Verdana", Sans-serif;
+        position: absolute;
+        top: 345%;
+        left: 19%;
+        font-weight: bold;
+        font-size: 30px;
+        color: rgb(255, 255, 255);
+}
+
+
+
+
+
+</style>
+
+
+<video width="400" class="video1" controls>
+    <source src="farming.mp4" type="video/mp4"/>
+    <track
+    src="farming/captions.vvt"
+    kind="captions"
+    srclang="eng"
+    label="en"
+    default
+    />
+
+</video>
+
+<video width="400" class="video2" controls>
+    <source src="fishinggame.mp4" type="video/mp4"/>
+    <track
+    src="fishinggame/captions.vvt"
+    kind="captions"
+    srclang="eng"
+    label="en"
+    default
+    />
+
+</video>
+
+<video width="400" class="video3" controls>
+    <source src="game.mp4" type="video/mp4"/>
+    <track
+    src="game/captions.vvt"
+    kind="captions"
+    srclang="eng"
+    label="en"
+    default
+    />
+
+</video>
+
+
+
+
+<div class= "background"></div>
+<h1>Games</h1>
+<div id="line1" class="line1">Over the past 2 years in college i have coded a few games. Some of these <br> were made using c++  and some using javascript.
+    Here i will show some short clips <br> of the gameplay and explain a bit about them!
+</div>
+<div id="line2" class="line2"> This is the first game i made using javascript. I made all of the art <br> in this game along with coding it myself.</div>
+<div id="line3" class="line3">This was my final game project in first year. I also made all of the art for this <br> game and i really enjoyed coding this game.</div>
+<div id="line4" class="line4">This was the final assignment for a different class in first year. I did not <br> make the art for this game but it was still enjoyable to code!</div>
