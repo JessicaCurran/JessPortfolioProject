a3c09fcbe717f22092dd55288e444457ee58e908
unknown <c00300855@setu.ie>
2025-05-04 23:24:30 +0100

    fixing live push changed videos to images to check if work

--- svelte-app/src/routes/Games/+page.svelte
+++ svelte-app/src/routes/Games/+page.svelte
@@ -30,12 +30,7 @@ h1 {
         color: rgb(255, 255, 255);
 }
 
-    .video1 {
-        width: 1000px;
-        position:absolute;
-        top: 75%;
-        left: 25%;
-    }
+
     
     .line2 {
         font-family: "Verdana", Sans-serif;
@@ -47,12 +42,7 @@ h1 {
         color: rgb(255, 255, 255);
 }
 
-    .video2 {
-        width: 1000px;
-        position:absolute;
-        top: 175%;
-        left: 25%;
-    }
+
 
     .line3 {
         font-family: "Verdana", Sans-serif;
@@ -64,12 +54,6 @@ h1 {
         color: rgb(255, 255, 255);
 }
 
-.video3 {
-        width: 1000px;
-        position:absolute;
-        top: 275%;
-        left: 25%;
-    }
 
     .line4 {
         font-family: "Verdana", Sans-serif;
@@ -82,47 +66,54 @@ h1 {
 }
 
 
+.game2 {
+         position: absolute;
+         top: 70%;
+         right: 18%;
+         width: 1200px;
+         z-index: 1;
+         transition: transform 0.3s ease;
+        
+    }
+
+    .game2:hover {
+        transform:scale(1.3);
+    }
+
+    
+.game3 {
+         position: absolute;
+         top: 168%;
+         right: 28%;
+         width: 777px;
+         z-index: 1;
+         transition: transform 0.3s ease;
+        
+    }
+
+    .game3:hover {
+        transform:scale(1.3);
+    }
+
+    .game1 {
+         position: absolute;
+         top: 265%;
+         right: 28%;
+         width: 780px;
+         z-index: 1;
+         transition: transform 0.3s ease;
+        
+    }
+
+    .game1:hover {
+        transform:scale(1.3);
+    }
 
 
 
 </style>
 
 
-<video width="400" class="video1" controls>
-    <source src="farming.mp4" type="video/mp4"/>
-    <track
-    src="farming/captions.vvt"
-    kind="captions"
-    srclang="eng"
-    label="en"
-    default
-    />
-
-</video>
-
-<video width="400" class="video2" controls>
-    <source src="fishinggame.mp4" type="video/mp4"/>
-    <track
-    src="fishinggame/captions.vvt"
-    kind="captions"
-    srclang="eng"
-    label="en"
-    default
-    />
-
-</video>
-
-<video width="400" class="video3" controls>
-    <source src="game.mp4" type="video/mp4"/>
-    <track
-    src="game/captions.vvt"
-    kind="captions"
-    srclang="eng"
-    label="en"
-    default
-    />
-
-</video>
 
 
 
@@ -135,3 +126,6 @@ h1 {
 <div id="line2" class="line2"> This is the first game i made using javascript. I made all of the art <br> in this game along with coding it myself.</div>
 <div id="line3" class="line3">This was my final game project in first year. I also made all of the art for this <br> game and i really enjoyed coding this game.</div>
 <div id="line4" class="line4">This was the final assignment for a different class in first year. I did not <br> make the art for this game but it was still enjoyable to code!</div>
+<img src= "game1.png" alt= "game drawing" class= "game1"/>
+<img src= "game2.png" alt= "game drawing" class= "game2"/>
+<img src= "game3.png" alt= "game drawing" class= "game3"/>
