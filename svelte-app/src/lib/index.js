ea9b35bbb3c9884bb3ad5ecb8404dfe8ad6eaa62
unknown <c00300855@setu.ie>
2025-05-04 13:38:31 +0100

    added prototype files, index js still weird not sure how to fix

--- svelte-app/src/lib/index.js
+++ svelte-app/src/lib/index.js
@@ -1,6 +1,4 @@
-
 import Header from "./components/Header.svelte";
 
 export {default as Navigation} from "./components/Navigation.svelte";
 export {default as Header} from "./components/Header.svelte";
-/* I've tried everything to fix these errors but nothing has worked /*
